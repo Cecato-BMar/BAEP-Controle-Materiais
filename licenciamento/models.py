f508d48bd1fa10c3a3207067415e3b42a06e3398
Cecato-BMar <129400192+Cecato-BMar@users.noreply.github.com>
2026-05-13 09:30:56 -0300

    fix: remove unique constraint from client_id to allow license renewals
    - Updated LicenseRecord model to remove unique=True from client_id
    - Added migration to apply the change to the database
    - This fixes the IntegrityError when activating a new license for the same client.

--- licenciamento/models.py
+++ licenciamento/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class LicenseRecord(models.Model):
-    client_id = models.CharField(max_length=100, unique=True, verbose_name="ID do Cliente")
+    client_id = models.CharField(max_length=100, verbose_name="ID do Cliente")
     client_name = models.CharField(max_length=200, verbose_name="Nome do Cliente")
     token_base64 = models.TextField(verbose_name="Token Criptográfico")
     issued_at = models.DateTimeField(verbose_name="Data de Emissão")
