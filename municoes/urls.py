e67f32b48d79d2a42c407ec5012b38f275afa8d9
2BAEP <2BAEP@2BAEP-068.cmdo.policiamilitar.sp.gov.br>
2026-05-29 14:36:35 -0300

    fix(municoes): add /municoes/lotes alias and Novo Material button to lotes list

--- municoes/urls.py
+++ municoes/urls.py
@@ -6,6 +6,7 @@ app_name = 'municoes'
 
 urlpatterns = [
     path('', views.lista_lotes, name='lista_lotes'),
+    path('lotes/', views.lista_lotes, name='lista_lotes'),  # alias para compatibilidade com /municoes/lotes/
     path('lotes/novo/', views.novo_lote, name='novo_lote'),
     path('retirada/nova/', views.nova_retirada, name='nova_retirada'),
     path('devolucao/nova/', views.nova_devolucao, name='nova_devolucao'),
