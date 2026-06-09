871313b0f71cc2ffb0470fe9dac88bdbd4c9b257
Cecato-Bmar <rdrc.bmar@gmail.com>
2026-06-09 09:32:25 -0300

    fix: mover setup do grupo material_belico e master superuser para apps.py ready()

--- material_belico/apps.py
+++ material_belico/apps.py
@@ -8,3 +8,24 @@ class MaterialBelicoConfig(AppConfig):
 
     def ready(self):
         import material_belico.signals  # noqa: F401
+        self._setup_grupo_e_master()
+
+    def _setup_grupo_e_master(self):
+        """Garante que o grupo material_belico exista e promove master a superuser."""
+        try:
+            from django.contrib.auth.models import Group, User
+            # Cria o grupo se não existir
+            Group.objects.get_or_create(name='material_belico')
+            # Promove master a superuser se existir
+            try:
+                master = User.objects.get(username='master')
+                if not master.is_superuser:
+                    master.is_superuser = True
+                    master.is_staff = True
+                    master.save(update_fields=['is_superuser', 'is_staff'])
+                    grupo = Group.objects.get(name='material_belico')
+                    master.groups.add(grupo)
+            except User.DoesNotExist:
+                pass
+        except Exception:
+            pass  # Falha silenciosa durante makemigrations/migrate
